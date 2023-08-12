cb98270898def4e3d8da2e164bf37bed74f18a1b
Aya Hassan <ayahassan.ah74@gmail.com>
2023-08-11 19:46:56 -0700

    C01 Commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, x;
+
+	for (a = 48; a <= 56; a += 1)
+	{
+		for (x = 49; x <= 57; x += 1)
+		{
+			if (x > a)
+			{
+				putchar(a);
+				putchar(x);
+				if (a != 56 || x != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
