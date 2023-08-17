2b24b3b159e3bf1521477851ad5e87942bc148e2
Aya Hassan <ayahassan.ah74@gmail.com>
2023-08-17 04:33:15 -0700

    C02 Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,20 +10,26 @@ void times_table(void)
 
 	for (a = 0; a < 10; a += 1)
 	{
-		_putchar('0');
 		for (b = 0; b < 10; b += 1)
 		{
-			_putchar(' ');
-			_putchar(',');
-
 			c = a * b;
-
-			if (c < 10)
+			if (b == 0)
 			{
-				_putchar(' ');
+				_putchar(c + '0');
 			}
-			else
+
+			if (c < 10 && b != 0)
 			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
 			}
