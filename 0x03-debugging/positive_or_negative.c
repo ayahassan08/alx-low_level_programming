65fb1ca2875197a0788b9159da8adb27608af9a3
Aya Hassan <ayahassan.ah74@gmail.com>
2023-08-17 14:49:16 -0700

    C03 Commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * positive_or_negative - gives the correct output when given a case of 0
+ * @i: the int to be checked
+ */
+
 void positive_or_negative(int i)
 {
 	if (i > 0)
