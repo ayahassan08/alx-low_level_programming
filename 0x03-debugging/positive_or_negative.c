5292fdf56087d16513b0eed1f240f06cd4daa267
Aya Hassan <ayahassan.ah74@gmail.com>
2023-08-17 14:35:42 -0700

    C03 Commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%i is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%i is negative\n", i);
+	}
+	else
+	{
+		printf("%i is zero\n", i);
+	}
+}
