da727e4b1b56a946ddda831234bb8e72ac14b0b6
Aya Hassan <ayahassan.ah74@gmail.com>
2023-08-18 06:30:15 -0700

    C03 Commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: the result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
