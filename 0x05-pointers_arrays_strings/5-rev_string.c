ffd6df296cdff929b70606724b82f52649f59c3f
Aya Hassan <ayahassan.ah74@gmail.com>
2023-08-21 20:16:25 -0700

    C05 Commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,23 +7,18 @@
 
 void rev_string(char *s)
 {
-	int a = 0;
-	int b;
-	int x = 0;
+	int a, b;
+	char x = s[0];
 
-	while (s[a] != '\0')
+	for (a = 0; s[a] != '\0'; a += 1)
 	{
-		a += 1;
+		;
 	}
 	for (b = 0; b < a; b += 1)
 	{
 		a--;
-
-		while (s[x])
-		{
-			x = s[b];
-			s[b] = s[a];
-			s[a] = x;
-		}
+		x = s[b];
+		s[b] = s[a];
+		s[a] = x;
 	}
 }
