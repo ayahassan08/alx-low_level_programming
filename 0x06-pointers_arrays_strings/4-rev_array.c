d4467a70f22566983ddfa9dae5d21002abaab927
ayahassan08 <ayahassan.ah74@gmail.com>
2023-08-25 04:56:18 +0300

    C06 Commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements of the array
+ * Return: Always 0 (Success)
+*/
+
+void reverse_array(int *a, int n)
+{
+	int first = 0;
+	int last = n - 1;
+	int swap;
+
+	if (first < last)
+	{
+		swap = a[first];
+		a[first] = a[last];
+		a[last] = swap;
+		last += 1;
+		last -= 1;
+	}
+}
