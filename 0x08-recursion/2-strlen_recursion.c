25019c801d30175d05fb5b0d8a811013e040d9ac
ayahassan08 <ayahassan.ah74@gmail.com>
2023-08-31 08:52:16 +0300

    C08 Commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be checked
+ * Return: the length of the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length += 1;
+		length = length + _strlen_recursion(s + 1);
+	}
+	return (length);
+}
