865c551b5d8625e85cf9dedef0e68985ba9a3c53
ayahassan08 <ayahassan.ah74@gmail.com>
2023-09-02 04:46:56 +0300

    A00 Commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: integer to be checked
+ * @argv: string to be checked
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
