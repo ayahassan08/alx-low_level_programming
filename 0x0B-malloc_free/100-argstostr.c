1cf1affbfa216f208caa95baba12928f5bb4eb97
ayahassan08 <ayahassan.ah74@gmail.com>
2023-09-06 04:33:43 +0300

    B00 Commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: integer to be checked
+ * @av: string to be checked
+ * Return: NULL if ac == 0 or av == NULL
+ * Returns a pointer to a new string, or NULL if it fails
+*/
+
+char *argstostr(int ac, char **av)
+{
+	char *args;
+	int a, b, c, length = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < ac; a += 1)
+	{
+		for (b = 0; av[a][b]; b += 1)
+		{
+			length += 1;
+		}
+		length += 1;
+	}
+
+	args = malloc(sizeof(*args) * length);
+
+	if (args == NULL)
+	{
+		return (NULL);
+	}
+
+	c = 0;
+
+	for (a = 0; a < ac; a += 1)
+	{
+		for (b = 0; av[a][b]; b += 1)
+		{
+			args[c++] = av[a][b];
+		}
+		args[c++] = '\n';
+	}
+	
+	return (args);
+}
