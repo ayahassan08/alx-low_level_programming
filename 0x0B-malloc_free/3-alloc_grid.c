d62feff2de350b53d6e5433aa92a445877856141
ayahassan08 <ayahassan.ah74@gmail.com>
2023-09-06 04:06:06 +0300

    B00 Commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,39 +12,33 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid;
-	int x, o, i;
+	int i, j;
 
+	/* check for invalid arguments */
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
 
+	/* allocate memory for the array of pointers */
 	grid = malloc(sizeof(int *) * height);
-
 	if (grid == NULL)
-	{
 		return (NULL);
-	}
-	for (x = 0; x < height; x += 1)
-	{
-		grid[x] = malloc(sizeof(int) * width);
 
-		if (grid[x] == NULL)
+	/* allocate memory for each row and initialize to 0 */
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 		{
-			while (x -= 1)
-			{
-				free(grid[x]);
-			}
+			/* free the allocated memory if allocation fails */
+			while (i--)
+				free(grid[i]);
 			free(grid);
 			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
-	for (i = 0; i < height; i += 1)
-	{
-		for (o = 0; o < width; o += 1)
-		{
-			grid[i][o] = 0;
-		}
-	}
+
+	/* return the pointer to the 2D array */
 	return (grid);
 }
