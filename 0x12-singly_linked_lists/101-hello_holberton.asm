d018db0201735a304b9b3e5aa0e08435aea0c273
ayahassan08 <ayahassan.ah74@gmail.com>
2023-09-22 00:25:22 +0300

    C12 Commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,26 @@
-; Write a 64-bit program in assembly that prints
-; Hello, Holberton, followed by a new line.
+; File: 101-hello_holberton.asm
+; Auth: Brennan D Baraban
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
 
 extern printf
 
 section .text
-	global main
+   global main
 
 main:
-	sub     rsp, 8
-	mov     rdi, format
-	mov     rsi, advanced
-	call    printf
-	add     rsp, 8
-	ret
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
 
 section .data
-	advanced db "Hello, Holberton", 0
-	format db "%s", 10, 0
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
