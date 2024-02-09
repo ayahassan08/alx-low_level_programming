ded129c3a300c7b341fb4133f16dbabfa42d9475
ayahassan08 <ayahassan.ah74@gmail.com>
2024-02-09 18:00:24 +0200

    C14 Commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	int x;
+	unsigned int result = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (x = 0; b[x]; x += 1)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[x] < '0' || b[x] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		result = 2 * result + (b[x] - '0');
 	}
 
-	return (dec_val);
+	return (result);
 }
-
