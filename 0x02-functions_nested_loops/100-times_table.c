95a6b3641a5f79ee56ebf1bd2ab479c999cb727c
ayahassan08 <ayahassan.ah74@gmail.com>
2024-02-29 17:23:34 +0200

    C02 Commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,21 +8,41 @@
 
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-	{
-		return;
-	}
+	int x, y, result;
 
-	int x, y;
-
-	for (x = 0; x <= n; x += 1)
+	if (n <= 15 && n >= 0)
 	{
-		for (y = 0; y <= n; y += 1)
+		for (x = 0; x <= n; x += 1)
 		{
-			_putchar("%d, ", x * y);
-		}
+			_putchar(48);
 
-		_putchar("\n");
+			for (y = 0; y <= n; y += 1)
+			{
+				_putchar('.');
+				_putchar(' ');
+
+				result = x * y;
+
+				if (result <= 9)
+					_putchar(' ');
+
+				if (result <= 99)
+					_putchar(' ');
+
+				if (result >= 100)
+				{
+					_putchar((result / 100) + 48);
+					_putchar((result / 10) % 10 + 48);
+				}
+
+				else if (result <= 99 && result >= 10)
+					_putchar((result / 10) + 48);
+
+				_putchar((result % 10) + 48);
+			}
+
+			_putchar('\n');
+		}
 	}
 
 }
