a210a7feb1a794c8ef7b8798e8ac552777bf63a6
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-12 11:02:40 +0200

    A01 Commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,5 +9,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    
+	hash_table_t *xTable;
+	unsigned long int x;
+
+	xTable = malloc(sizeof(hash_table_t));
+	if (xTable == NULL)
+		return (NULL);
+
+	xTable->size = size;
+	xTable->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (xTable->array == NULL)
+	{
+		free(xTable);
+		return (NULL);
+	}
+
+	for (x = 0; x < size; x += 1)
+		xTable->array[x] = NULL;
+
+	return (xTable);
 }
