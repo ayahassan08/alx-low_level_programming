a3fb90d78ec4867aa7cf6243a0678b06587c6a8d
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-12 10:51:42 +0200

    A01 Commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - creates a hash table
+* @size: he size of the array
+* Return: a pointer to the newly created hash table
+* If something went wrong, your function should return NULL
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    
+}
